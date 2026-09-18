399d3441a7cc7dc1f1793ca71eb2031ba0112fdb
Bacon <ralphsabandal02@gmail.com>
2026-09-18 19:56:29 +0000

    Add temporary student database test

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,19 +9,27 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StaffController;
 use App\Http\Controllers\MedicineController;
 
-
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/test-students', function () {
+    return response()->json(
+        \App\Models\Student::count()
+    );
+});
 
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
     Route::put('/profile', [AuthController::class, 'updateProfile']);
+
     Route::get('/dashboard', [DashboardController::class, 'index']);
+
     Route::apiResource('students', StudentController::class);
     Route::apiResource('staff', StaffController::class);
-    
+
     Route::apiResource('clinic-visits', ClinicVisitController::class);
-    route::get('/nurses', [ClinicVisitController::class, 'nurses']);
+    Route::get('/nurses', [ClinicVisitController::class, 'nurses']);
+
     Route::apiResource('medicines', MedicineController::class);
 });
